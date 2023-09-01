35a8622ea371ccbcd1cc79654b860c4c8301b2a5
Igor Zinkovsky <igor@deno.com>
2023-09-01 12:29:49 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -10,6 +10,6 @@ import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
 
-await start(manifest, config);
-
 await foo();
+
+await start(manifest, config);
